8743c7ebed3fceb36c670ab54ce9c851eabeac27
liuzhxian <liu_zhxian@163.com>
2019-03-08 00:55:33 +0800

    huk

--- src/main/java/com/jt/web/controller/CartController.java
+++ src/main/java/com/jt/web/controller/CartController.java
@@ -21,7 +21,7 @@ import com.jt.web.thread.UserThreadLocal;
 public class CartController {
 	
 	@Autowired
-	private CartService cartServicesjki;
+	private CartService cartServicespli;
 	
 	//1.实现购物车展现
 	@RequestMapping("/show")
